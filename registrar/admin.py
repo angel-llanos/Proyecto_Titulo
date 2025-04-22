dc3dc1f95539057e445750d6231f28c56706b86c
pntrkllr <rlrc.262@gmail.com>
2025-04-21 22:38:52 -0400

    roles-usuarios

--- registrar/admin.py
+++ registrar/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'rol', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información adicional', {'fields': ('rol',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Información adicional', {'fields': ('rol',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- registrar/forms.py
+++ registrar/forms.py
@@ -1,13 +1,21 @@
+# forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser  # ahora usamos el modelo que creaste
 
 class CustomUserCreationForm(UserCreationForm):
-     first_name = forms.CharField(label='Nombre', max_length=30, required=True)
-     last_name = forms.CharField(label='Apellido', max_length=30, required=True)
-     email = forms.EmailField(label='Correo electrónico', required=True)
-     username = forms.CharField(label='Nombre de usuario', max_length=150)
-     
-     class Meta:
-          model = User
-          fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+    first_name = forms.CharField(label='Nombre', max_length=30, required=True)
+    last_name = forms.CharField(label='Apellido', max_length=30, required=True)
+    email = forms.EmailField(label='Correo electrónico', required=True)
+    username = forms.CharField(label='Nombre de usuario', max_length=150)
+
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.rol = 1  # Cliente por defecto
+        if commit:
+            user.save()
+        return user
